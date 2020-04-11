f563a18ea27fbe7d85d2a668951174338e7c2e4e
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2020-04-11 18:23:51 +0545

    Changes: Added PhpStorm and WebStorm Open with Context menu Command Script.

---
+++ WebStorm - Installed via intellij Toolbox.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+:: change the path below to match your installed version
+SET WebStormPath=C:\Users\anuzb\AppData\Local\JetBrains\Toolbox\apps\WebStorm\ch-0\201.6668.106\bin\webstorm64.exe
+ 
+echo Adding file entries
+@reg add "HKEY_CLASSES_ROOT\*\shell\WebStorm" /t REG_SZ /v "" /d "Open with WebStorm"   /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\WebStorm" /t REG_EXPAND_SZ /v "Icon" /d "%WebStormPath%,0" /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\WebStorm\command" /t REG_SZ /v "" /d "%WebStormPath% \"%%1\"" /f
+ 
+echo Adding within a folder entries
+@reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\WebStorm" /t REG_SZ /v "" /d "Open with WebStorm"   /f
+@reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\WebStorm" /t REG_EXPAND_SZ /v "Icon" /d "%WebStormPath%,0" /f
+@reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\WebStorm\command" /t REG_SZ /v "" /d "%WebStormPath% \"%%V\"" /f
+
+echo Adding folder entries
+@reg add "HKEY_CLASSES_ROOT\Directory\shell\WebStorm" /t REG_SZ /v "" /d "Open with WebStorm"   /f
+@reg add "HKEY_CLASSES_ROOT\Directory\shell\WebStorm" /t REG_EXPAND_SZ /v "Icon" /d "%WebStormPath%,0" /f
+@reg add "HKEY_CLASSES_ROOT\Directory\shell\WebStorm\command" /t REG_SZ /v "" /d "%WebStormPath% \"%%1\"" /f
+
+pause
